cb014cdaa8328153b72c87db36c1328dc645aee7
ukkit <neeraj.tikku@gmail.com>
2019-05-09 12:43:17 +0530

    generating seperate TEXT, CSV & SQL files

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # get-navall
+
 PHP script to download NAVAll.txt file from ampfiindia site as CSV and convert the same to SQL file.
 
 ## How to
+
 Download index.php file and run it wither via browser:
+
 > http://you-local-site/get-navall
 
 Or you can also run it through command prompt/terminal:
+
 > php -f index.php
 
 ## Output
@@ -13,14 +17,36 @@ Or you can also run it through command prompt/terminal:
 By default, the script saves NAVAll.txt file as .CSV file without making any changes. The file is saved as:
 
 ```php
-    "/nav-" . date('dmY') . ".csv";
+    "/nav-" . date('dmY') . ".txt";
     // nav-07052019.csv
 ```
-The script also converts the CSV into an SQL file.  In the SQL file, a table is created with name of CSV file:
+
+The script then converts the TEXT into CSV & SQL files.
+
+### CSV file
+
+CSV file is generated with header and rows:
+
+> "Scheme Type, Fund Family, Scheme Code, ISIN Div Payout/ISIN Growth, ISIN Div Reinvestment, Scheme Name, Net Asset Value, Date"
+>
+> "Open Ended Schemes(Debt Scheme - Banking and PSU Fund),Aditya Birla Sun Life Mutual Fund,119551,INF209KA12Z1,INF209KA13Z9,Aditya Birla Sun Life Banking & PSU Debt Fund - Direct Plan-Dividend,143.1504,08-May-2019"
+>
+> "Open Ended Schemes(Debt Scheme - Banking and PSU Fund),Aditya Birla Sun Life Mutual Fund,119552,INF209K01YM2,NULL,Aditya Birla Sun Life Banking & PSU Debt Fund - Direct Plan-Monthly Dividend,107.0014,08-May-2019"
+
+You can disable generation of CSV file by changing following variable to 'false' in index.php file:
+
+```php
+    $GENERATE_CSV = false;
+```
+
+### SQL file
+
+In the SQL file, a table is created with name of CSV file:
 
 ```sql
     CREATE TABLE IF NOT EXISTS `NAV-07052019`
 ```
+
 The table definition is based on different CSV headings:
 
 ```sql
@@ -36,11 +62,11 @@ The table definition is based on different CSV headings:
         `date` varchar(12) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ```
+
 Resulting insert statement:
 
 ```sql
-    INSERT INTO `NAV-07052019` (`ID`, `scheme_type`,`fund_family`,`scheme_code`, `isin_div_payout`, `isin_div_reinvest`, `scheme_name`, `nav`, `date`) VALUES 
-    
+    INSERT INTO `NAV-07052019` (`ID`, `scheme_type`,`fund_family`,`scheme_code`, `isin_div_payout`, `isin_div_reinvest`, `scheme_name`, `nav`, `date`) VALUES
     (NULL,'Open Ended Schemes(Debt Scheme - Banking and PSU Fund)','Aditya Birla Sun Life Mutual Fund',119551,'INF209KA12Z1','INF209KA13Z9','Aditya Birla Sun Life Banking & PSU Debt Fund  - Direct Plan-Dividend',143.0201,'06-May-2019');
 ```
 
@@ -52,7 +78,24 @@ You can disable generation of SQL by changing following variable to 'false' in i
 
 ---
 
+## Others
+
+- Define seperate output directories:
+
+```php
+    $txt_dir = dirname(__FILE__);
+    $csv_dir = dirname(__FILE__);
+    $sql_dir = dirname(__FILE__);
+```
+
+- Keep the TEXT file downloaded:
+
+```php
+    $KEEP_TXT = true;
+```
+
+---
+
 Feel free to go through the code and change it.
 
 :india:
-
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
-// Change $csv_dir & $sql_dir 
-$csv_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR; 
-$sql_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 
 // Define below varible as true if you want to generate .sql file from .csv file
+$GENERATE_CSV = true;
 $GENERATE_SQL = true;
+$KEEP_TXT = false;
 
-// Generating CSV file name
-$csv_file = $csv_dir . "/nav-" . date('dmY') . ".csv";
+// Change $csv_dir & $sql_dir
+$txt_dir = dirname(__FILE__);
+$csv_dir = dirname(__FILE__);
+$sql_dir = dirname(__FILE__);
+
+// Generating TEXT file name
+$txt_file = $txt_dir . "/nav-" . date('dmY') . ".txt";
 
 // defining curl url
 $ch = curl_init("https://www.amfiindia.com/spages/NAVAll.txt");
 
 // opening csv_file in write mode
-$fp = fopen($csv_file, "w");
+$fp = fopen($txt_file, "w");
 
 // setting curl options
 curl_setopt($ch, CURLOPT_FILE, $fp);
@@ -29,20 +33,28 @@ if (curl_exec($ch) === false) {
 curl_close($ch);
 fclose($fp);
 
-if (file_exists($csv_file)) {
-    echo $csv_file . " generated. <br>";
+if (file_exists($txt_file)) {
+    echo $txt_file . " generated. <br>";
 } else {
     echo "There was some problem in file generation, please check directory rights/permissions";
 }
 
 
-// generate SQL file based on CSV file if GENERATE_SQL flag is true
-if ($GENERATE_SQL) {
-    generateSQLFile($csv_file, $sql_dir);
+// generate CSV file based on TEXT file if GENERATE_CSV flag is true
+if ($GENERATE_CSV) {
+    generateSQLFile("CSV",$txt_file,$csv_dir);
 }
 
+// generate SQL file based on TEXT file if GENERATE_SQL flag is true
+if ($GENERATE_SQL) {
+    generateSQLFile("SQL",$txt_file,$sql_dir);
+}
 
-function generateSQLFile($inputfile, $sql_dir)
+if (!$KEEP_TXT) {
+    unlink($txt_file);
+}
+
+function generateSQLFile($file_type=null,$input_file=null,$dir_path=null)
 {
     // defining variable
     $par = 0;
@@ -52,37 +64,50 @@ function generateSQLFile($inputfile, $sql_dir)
     $child = null;
 
     // Get only file name from $inputfile
-    $filename = pathinfo($inputfile, PATHINFO_FILENAME);
-    $tablename = strtoupper(trim($filename));
+    $file_name = pathinfo($input_file, PATHINFO_FILENAME);
 
-    // sql_file name based on inputfile name
-    $sql_file = $sql_dir . "/" . $filename . ".sql";
+    if ($file_type == "CSV") {
+        // file name based on inputfile name
+        $gen_file = $dir_path . "/" . $file_name . ".csv";
+        file_put_contents($gen_file, ""); //this is to blank out existing .sql file
+        $csv_array = array();
 
-    file_put_contents($sql_file, ""); //this is to blank out existing .sql file
-    $fp = fopen($sql_file, 'w');
+        $fp = fopen($gen_file, 'w') or die("Can't open $gen_file");
 
-    $SQL = "CREATE TABLE IF NOT EXISTS `" . $tablename . "` (\n";
-    $SQL .= "`ID` int(11) NOT NULL,\n";
-    $SQL .= "`scheme_type` varchar(191) NOT NULL,\n";
-    $SQL .= "`fund_family` varchar(191) NOT NULL,\n";
-    $SQL .= "`scheme_code` int(11) NOT NULL,\n";
-    $SQL .= "`isin_div_payout` varchar(25) DEFAULT NULL,\n";
-    $SQL .= "`isin_div_reinvest` varchar(25) DEFAULT NULL,\n";
-    $SQL .= "`scheme_name` varchar(191) NOT NULL,\n";
-    $SQL .= "`nav` float(10,4) NOT NULL,\n";
-    $SQL .= "`date` varchar(12) NOT NULL\n";
-    $SQL .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n";
-    $SQL .= "\n";
+        $csv_array[0] = ("Scheme Type, Fund Family, Scheme Code, ISIN Div Payout/ISIN Growth, ISIN Div Reinvestment, Scheme Name, Net Asset Value, Date");
+    }
 
-    $SQL .= "ALTER TABLE `$tablename` ADD PRIMARY KEY (`ID`);\n";
-    $SQL .= "\n";
-    $SQL .= "ALTER TABLE `$tablename`\n";
-    $SQL .= "MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;\n";
-    $SQL .= "\n";
+    if ($file_type == "SQL") {
+        $tablename = strtoupper(trim($file_name));
 
-    fwrite($fp, $SQL);
+        // file name based on inputfile name
+        $gen_file = $dir_path . "/" . $file_name . ".sql";
+        file_put_contents($gen_file, ""); //this is to blank out existing .sql file
+        $fp = fopen($gen_file, 'w');
 
-    if (($handle = fopen($inputfile, "r")) !== false) {
+        $SQL = "CREATE TABLE IF NOT EXISTS `" . $tablename . "` (\n";
+        $SQL .= "`ID` int(11) NOT NULL,\n";
+        $SQL .= "`scheme_type` varchar(191) NOT NULL,\n";
+        $SQL .= "`fund_family` varchar(191) NOT NULL,\n";
+        $SQL .= "`scheme_code` int(11) NOT NULL,\n";
+        $SQL .= "`isin_div_payout` varchar(25) DEFAULT NULL,\n";
+        $SQL .= "`isin_div_reinvest` varchar(25) DEFAULT NULL,\n";
+        $SQL .= "`scheme_name` varchar(191) NOT NULL,\n";
+        $SQL .= "`nav` float(10,4) NOT NULL,\n";
+        $SQL .= "`date` varchar(12) NOT NULL\n";
+        $SQL .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n";
+        $SQL .= "\n";
+
+        $SQL .= "ALTER TABLE `$tablename` ADD PRIMARY KEY (`ID`);\n";
+        $SQL .= "\n";
+        $SQL .= "ALTER TABLE `$tablename`\n";
+        $SQL .= "MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;\n";
+        $SQL .= "\n";
+
+        fwrite($fp, $SQL);
+    }
+
+    if (($handle = fopen($input_file, "r")) !== false) {
         while (($data = fgetcsv($handle, 1000, ";")) !== false) {
 
             $num = count($data);
@@ -101,28 +126,44 @@ function generateSQLFile($inputfile, $sql_dir)
             } elseif ($num >= 2) {
                 if ($parent != "") {
                     $row++;
-                    $SQL = "INSERT INTO `$tablename` (`ID`, `scheme_type`,`fund_family`,`scheme_code`, `isin_div_payout`, `isin_div_reinvest`, `scheme_name`, `nav`, `date`) VALUES (NULL,'" . $parent . "','" . $child . "'";
+                    $parent = str_replace("'", "\'",$parent);
+                    $child = str_replace("'", "\'", $child);
+                    if ($file_type == "SQL") {
+                        $ROW = "INSERT INTO `$tablename` (`ID`, `scheme_type`,`fund_family`,`scheme_code`, `isin_div_payout`, `isin_div_reinvest`, `scheme_name`, `nav`, `date`) VALUES (NULL,'" . $parent . "','" . $child . "'";
+                    } else {
+                        $ROW = $parent . "," . $child;
+                    }
                     for ($c = 0; $c < $num; $c++) {
                         if ($c == 0) {
-                            $SQL .= "," . trim($data[$c]);
+                            $ROW .= "," . trim($data[$c]);
                         } elseif ($c == 4) {
                             if ($data[$c] == "N.A.") {
-                                $SQL .= ",0.0";
+                                $ROW .= ",0.0";
                             } else {
-                                $SQL .= "," . trim($data[$c]);
+                                $ROW .= "," . trim($data[$c]);
                             }
                         } else {
                             if (trim($data[$c]) == '-') {
-                                $SQL .= ",NULL";
+                                $ROW .= ",NULL";
                             } else {
                                 $text = str_replace("'", "\'", trim($data[$c]));
-                                $SQL .= ",'" . $text . "'";
+                                $text = str_replace(",", "", $text);
+                                $ROW .= ",'" . $text . "'";
                             }
                         }
                     }
                     $subpar = 0;
-                    $SQL .= ");\n";
-                    fwrite($fp, $SQL);
+                    if ($file_type == "SQL") {
+                        $ROW .= ");\n";
+                        fwrite($fp, $ROW);
+                    } else {
+                        $line = preg_replace('/(^|;)"([^"]+)";/','$1$2;',$ROW);
+                        $line1 = str_replace("'","", $ROW);
+                        $line = str_replace('"','', $line1);
+                        array_push($csv_array, $line);
+                        fputcsv($fp, $csv_array);
+                        $csv_array = [];
+                    }
                 }
             }
         }
@@ -130,8 +171,8 @@ function generateSQLFile($inputfile, $sql_dir)
         fclose($fp);
     }
 
-    if (file_exists($sql_file)) {
-        echo $sql_file . " generated. <br>";
+    if (file_exists($gen_file)) {
+        echo $gen_file . " generated. <br>";
     } else {
         echo "There was some problem in file generation, please check directory rights/permissions";
     }
